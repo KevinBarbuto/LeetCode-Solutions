7683243a64494e2c6f759e06f648e275cd80a407
Kevin Barbuto <77207271+KevinBarbuto@users.noreply.github.com>
2022-07-29 12:49:26 -0400

    Create 13 - Roman to Arabic Numerals.py

---
+++ algorithms/13 - Roman to Arabic Numerals.py
@@ -0,0 +1,24 @@
+# Objective: Convert from Roman numerals to Arabic numerals
+
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        
+        # Hash table
+        roman = { "I" : 1, "V" : 5, "X" : 10, "L" : 50,
+                  "C" : 100, "D" : 500, "M" : 1000}
+        
+        result = 0 # Placeholder value for the conversion
+        
+        # For each symbol, subtract values smaller than next one
+        # Add symbols bigger or equal in value to next
+        for i in range(len(s)):
+            if i + 1 < len(s) and roman[s[i]] < roman[s[i + 1]]:
+                result -= roman[s[i]]
+                
+            else:
+                result += roman[s[i]]
+            
+        return result
+
+obj = Solution()
+print(obj.romanToInt(input("Enter any Roman numeral: ")))
