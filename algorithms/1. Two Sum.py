d64dd4bc420774f2faf2f82373fc15248a9d1f07
Kevin Barbuto <77207271+KevinBarbuto@users.noreply.github.com>
2022-12-23 20:25:52 -0500

    Update 1. Two Sum.py

--- algorithms/1. Two Sum.py
+++ algorithms/1. Two Sum.py
@@ -1,6 +1,9 @@
 # Objective: Given an input of an array and an integer, determine the indices of the two values in the array that will add up to the integer.
 # Constraint: Assume only one valid solution.
 
+# Note: This probably isn't the most optimal to this problem, as it brute-force searches the entire list. 
+# If I were to optimize this, I would probably write it to compare the target with each entry in the list so that the code doesn't have to potentially search through the entire list twice.
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
 
