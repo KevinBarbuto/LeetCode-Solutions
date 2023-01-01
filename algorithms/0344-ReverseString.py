110ef12cf1983cded7d167522f41e973c8d28fa1
Kevin Barbuto <77207271+KevinBarbuto@users.noreply.github.com>
2023-01-01 13:59:44 -0500

    Update 0344-ReverseString.py

--- algorithms/0344-ReverseString.py
+++ algorithms/0344-ReverseString.py
@@ -1,12 +1,10 @@
-# Objective: Take a string and given string and modify it to become reversed.
+# Objective: Take a given string and modify it to become reversed.
 # Constraint: For some reason, LeetCode will accept the trivial solution of simply writing "s.reverse()" and calling it a day. 
 # I felt this was too boring, so I made it more challenging on myself and attempted more of a stack-based solution.
 
 class Solution:
     def reverseString(self, s: List[str]) -> None:
-        """
-        Do not return anything, modify s in-place instead.
-        """
+        
         j = 0
         stack = [] # Initialize a stack to push / pull from.
         for i in s:
