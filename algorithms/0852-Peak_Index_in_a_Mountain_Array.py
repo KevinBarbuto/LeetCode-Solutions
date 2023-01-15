fc3fc16df8c36364ae0ebf5faa3170abdca2dfb8
Kevin Barbuto <77207271+KevinBarbuto@users.noreply.github.com>
2023-01-15 12:54:12 -0500

    Create 0852-Peak_Index_in_a_Mountain_Array.py

---
+++ algorithms/0852-Peak_Index_in_a_Mountain_Array.py
@@ -0,0 +1,8 @@
+# Objective: Return the index of the peak in a "mountain" array
+# (in which the data points form the shape of a single spike)
+
+class Solution:
+    def peakIndexInMountainArray(self, arr: List[int]) -> int:
+        for i in range( len(arr) ):
+            if arr[i] > arr[i+1]:
+                return (i)
