62950ee1f4997400517486ba63a0f38e64585cdd
Kevin Barbuto <77207271+KevinBarbuto@users.noreply.github.com>
2023-01-16 19:16:23 -0500

    Create 0412-Fizz_Buzz.js

---
+++ javascript/0412-Fizz_Buzz.js
@@ -0,0 +1,25 @@
+// Objective: Create a list of numbers that prints out "fizz" if the number is divisible by 3, 
+// "buzz" if the number is divisible by 5, and "fizzbuzz" if the number is divisible by both.
+
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var fizzBuzz = function(n) {
+    result = []
+
+    for (i=1; i<=n; i++) {
+        // Check for case where it's divisible by 3 and 5
+        if (i%15 == 0) {
+            result.push('FizzBuzz')
+        } else if (i%3 == 0) {
+            result.push('Fizz')
+        } else if (i%5 == 0) {
+            result.push('Buzz')
+        } else {
+            result.push( i.toString() ) // Convert to string
+        }
+    }
+
+    return result;
+};
